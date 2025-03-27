319baf8eb64c9c0b3276e0b7319080390a99895c
Rolazar <rolazarcoding@gmail.com>
2025-03-27 11:55:53 -0700

    updated gitignore

--- backend/src/routes/root.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export function root(request: Request, response: Response) {
-  const { url } = request;
-  response.send(
-    `<h1>Express Template</h1><p>Welcome to the backend page. Your path is ${url}</p>`
-  );
-}
