efad2844ff3c83e4879378ae28a5239ab9fc9219
Rolazar <rolazarcoding@gmail.com>
2025-03-31 13:20:26 -0700

    Added serverless support

--- backend/webpack.config.ts
+++ backend/webpack.config.ts
@@ -1,11 +1,12 @@
 import { Configuration } from "webpack";
+import Zip from "zip-webpack-plugin";
 
 export default {
   module: {
     rules: [
       {
-        test: /\.(js|ts)/, // Compile these files with babel-loader
-        exclude: /.(json)/, // Don't combile json files with babel-loader
+        test: /\.(js|ts)$/, // Compile these files with babel-loader
+        // exclude: /.(json)/, // Don't combile json files with babel-loader
         use: "babel-loader",
       },
     ],
@@ -16,4 +17,9 @@ export default {
     extensions: [".js", ".ts"], // Automatically inserts file extensions in import statements
   },
   target: "node",
+  output: {
+    filename: "index.js",
+    library: { type: "commonjs" },
+  },
+  plugins: [new Zip({ filename: "index.zip" })],
 } as Configuration;
